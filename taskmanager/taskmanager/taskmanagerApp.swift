6c6b6d0d12145b3648d7a7e5a47640ffa05489ab
Debbie Yuen <deborahyuen2016@berkeley.edu>
2025-04-14 09:51:54 -0700

    push apple vision pro project

---
+++ taskmanager/taskmanager/taskmanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskmanagerApp.swift
+//  taskmanager
+//
+//  Created by Debbie Yuen on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct taskmanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ taskmanager/taskmanagerTests/taskmanagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  taskmanagerTests.swift
+//  taskmanagerTests
+//
+//  Created by Debbie Yuen on 4/14/25.
+//
+
+import Testing
+@testable import taskmanager
+
+struct taskmanagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
